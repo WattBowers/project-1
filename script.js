a796d7bb37d52b99a7ea2f13aa4f85d019f06613
Jackson Watt-Bowers <wattbowers@gmail.com>
2023-02-19 14:52:49 -0500

    adding js functionality

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("hello")
+
+const gallerySection = document.getElementsByClassName('imageJsContainer')
+const leftArrow = document.getElementsByClassName('leftArrow');
+const rightArrow = document.getElementsByClassName('rightArrow')
+
+let count = 0;
+
+console.log(gallerySection)
+
+const imageArray = [
+    '<figure><img src="./assets/gallery-image-1.jpg" alt="A green house"></figure>',
+    '<figure><img src="./assets/gallery-image-2.jpg" alt="A collection of succulents"></figure>',
+    '<figure><img src="./assets/gallery-image-3.jpg" alt="Cactus and Succulents"></figure>'
+]
+
+gallerySection[0].innerHTML = imageArray[count];
+console.log(gallerySection)
+
+leftArrow[0].addEventListener('click', () => {
+    if(count !== 0) {
+        count -= 1; 
+        gallerySection[0].innerHTML = imageArray[count];
+    }
+
+})
+
+rightArrow[0].addEventListener('click', () => {
+    if(count !== imageArray.length -1) {
+        count += 1; 
+        gallerySection[0].innerHTML = imageArray[count];
+    }
+})
